a3eecaaba3c0431db024c09faf22f1e4eaaa8764
Matheus <matheuslsv7@gmail.com>
2022-07-29 15:11:01 -0300

    Criado componente de login e refatorado os estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
