1bc93561bbb22ce937a1f3c2c8f560171291b502
Matheus <matheuslsv7@gmail.com>
2022-08-03 18:54:25 -0300

    Criada a conexao com o back-end

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/CadastroUsuario" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
